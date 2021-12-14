f913d8f0b02cd2a5e4b65249a06528978904398a
Elthon Silva <eltonsilva.mecatronic@gmail.com>
2021-12-14 18:26:43 -0300

    Exercicios 1 e 2 e mudança de arquivos

--- src/br/com/dio/exercicios/loops/Ex1_NomeEIdade.java
+++ src/br/com/dio/exercicios/loops/Ex1_NomeEIdade.java
@@ -6,6 +6,26 @@ o primeiro representando o nome do aluno e o segundo representando a sua idade.
 (Pare o programa inserindo o valor 0 no campo nome)
  */
 
+import java.util.Scanner;
+
 public class Ex1_NomeEIdade {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		String nome;
+		int idade;
+
+		while (true) {
+			System.out.println("Nome: ");
+			nome = scan.next();
+			if(nome.equals("0")) break;
+
+			System.out.println("Idade: ");
+			idade = scan.nextInt();
+		}
+
+		System.out.println("Continua aqui...");
+
+	}
 
 }
---
+++ src/br/com/dio/exercicios/loops/Ex2_Nota.java
@@ -0,0 +1,27 @@
+package br.com.dio.exercicios.loops;
+/*
+Faça um programa que peça uma nota entre zero e dez.
+Mostre uma mensagem caso o valor seja inválido
+e continue pedindo
+até que o usuario informe um valor válido.
+ */
+
+import java.util.Scanner;
+
+public class Ex2_Nota {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		int nota;
+
+		System.out.println("Nota: ");
+		nota = scan.nextInt();
+
+		while(nota < 0 | nota > 10){
+			System.out.println("Nota invalida!! Digite novamente: ");
+			nota = scan.nextInt();
+		}
+	}
+
+}
